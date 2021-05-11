fba98a91cd0051d87b3bf1be1e6afbcdd48f059f
Luã Mourão <luan.gcc@gmail.com>
2021-05-11 18:57:20 -0300

    configuração do provider de uploadde imagens em produção (S3StorageProvider.ts); criação do use case de perfil do usuário (userProfileUseCase) para acesso das informações de seu cadastro, criação do DTO (IUserResponseDTO.ts) da response deste use case e do Mapper (UserMap.ts) para selecionar apenas as informações relevantes vindas no objeto User

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from "tsyringe";
+import { IStorageProvider } from "../../../../shared/container/providers/storageProvider/IStorageProvider";
 import { ICarsImagesRepository } from "../../repositories/ICarsImagesRepository";
-
 
 interface IRequest {
   car_id: string;
@@ -12,12 +12,16 @@ class UploadCarImagesUseCase {
 
   constructor(
     @inject("CarsImagesRepository")
-    private carsImagesRepository: ICarsImagesRepository
+    private carsImagesRepository: ICarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async (image) => {
       await this.carsImagesRepository.create(car_id, image);
+
+      await this.storageProvider.save(image, "cars");
     });
   };
 }
