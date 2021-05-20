18616757d1922a23dd7c2c953e1708ce932b9682
Luã Mourão <luan.gcc@gmail.com>
2021-05-20 18:02:14 -0300

    Configurações para rodar o banco de dados no Docker em produção

--- src/shared/infra/typeormDB/index.ts
+++ src/shared/infra/typeormDB/index.ts
@@ -5,10 +5,22 @@ export default async (host = "172.18.0.2"): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database: 
         process.env.NODE_ENV === "test" ? "rentx_test" : defaultOptions.database
     })
   );
 }
+
+// antes de subir a aplicação para produção
+// export default async (host = "172.18.0.2"): Promise<Connection> => {
+//   const defaultOptions = await getConnectionOptions();
+
+//   return createConnection(
+//     Object.assign(defaultOptions, {
+//       host: process.env.NODE_ENV === "test" ? "localhost" : host,
+//       database: 
+//         process.env.NODE_ENV === "test" ? "rentx_test" : defaultOptions.database
+//     })
+//   );
+// }
 
