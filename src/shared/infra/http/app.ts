ab3e7a0ff13ccc15a4f39e81c33a3bb3d7d98694
Luã Mourão <luan.gcc@gmail.com>
2021-05-26 14:07:28 -0300

    Configurando CORS

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,7 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerFile from '../../../swagger.json';
 import createConnection from '../typeormDB';
 import '../../container';
+import cors from 'cors';
 import { router } from './routes';
 import { AppError } from '../../errors/AppError';
 
@@ -20,9 +21,10 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
-// middleware para tratamento de exceções (erros)
+// middleware para tratamento de exceções
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({ message: err.message });
